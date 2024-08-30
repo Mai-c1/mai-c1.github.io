464612e015bd0fdf91fb07d867857e2b48a26248
Mai <2512834769@qq.com>
2024-08-30 16:32:37 +0800

    deploy

---
+++ assets/2_gongju.html-CUUyrsJW.js
@@ -0,0 +1,17 @@
+import{_ as s,o as n,c as e,b as a}from"./app-2hirHMK7.js";const t={},i=a(`<h1 id="工具" tabindex="-1"><a class="header-anchor" href="#工具"><span>工具</span></a></h1><h2 id="临时评测机" tabindex="-1"><a class="header-anchor" href="#临时评测机"><span>临时评测机</span></a></h2><div class="language-c++ line-numbers-mode" data-highlighter="prismjs" data-ext="c++" data-title="c++"><pre class="language-c++"><code><span class="line">#include &lt;bits/stdc++.h&gt;</span>
+<span class="line">using namespace std;</span>
+<span class="line"></span>
+<span class="line">int main() {</span>
+<span class="line">	int i = 0;</span>
+<span class="line">	while (i &lt; 1000) {</span>
+<span class="line">		system(&quot;data.exe &gt; data.txt&quot;);</span>
+<span class="line">		system(&quot;baoli.exe &lt; data.txt &gt; baoli.txt&quot;);</span>
+<span class="line">		system(&quot;test.exe &lt; data.txt &gt; test.txt&quot;);</span>
+<span class="line">		if (system(&quot;fc test.txt baoli.txt&quot;))</span>
+<span class="line">			break;</span>
+<span class="line">		else {</span>
+<span class="line">			cout &lt;&lt; &quot;success&quot; &lt;&lt; endl;</span>
+<span class="line">		}</span>
+<span class="line">	}</span>
+<span class="line">}</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),l=[i];function c(d,r){return n(),e("div",null,l)}const o=s(t,[["render",c],["__file","2_gongju.html.vue"]]),u=JSON.parse('{"path":"/notes/maiwenpeng/2_gongju.html","title":"工具","lang":"zh-CN","frontmatter":{"title":"工具","author":"Mai"},"headers":[{"level":2,"title":"临时评测机","slug":"临时评测机","link":"#临时评测机","children":[]}],"filePathRelative":"notes/麦文鹏/2_工具.md","git":{"createdTime":1723910784000,"updatedTime":1723957621000,"contributors":[{"name":"Mai","email":"2512834769@qq.com","commits":1}]}}');export{o as comp,u as data};
